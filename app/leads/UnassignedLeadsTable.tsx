0fd87beaf456aae44ce79c770974e35576f0b13e
wasique@dynsimulation.com <wasique@dynsimulation.com>
2026-08-27 12:03:53 +0530

    changes

--- app/leads/UnassignedLeadsTable.tsx
+++ app/leads/UnassignedLeadsTable.tsx
@@ -278,7 +278,7 @@ const UnassignedLeadsTable = ({
             className="flex items-center gap-2 py-2 px-4 rounded-[12px] border border-[#E7E7E7] bg-primary-600 hover:bg-primary-700 text-white text-sm font-medium transition cursor-pointer"
           >
             <FaSearchPlus className="w-4 h-4" />
-            <span>Search Leads</span>
+            <span>Search Unassigned Leads</span>
           </button>
         </div>
       </div>
